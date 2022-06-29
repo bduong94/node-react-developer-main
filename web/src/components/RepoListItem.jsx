4b6d65c2d435d1c9e1c123b710adf3c15cd39983
Brian Duong <brianminhduong@gmail.com>
2022-06-28 20:48:38 -0400

    Create test data for RepoListItem component

--- web/src/components/RepoListItem.jsx
+++ web/src/components/RepoListItem.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+// Include the repository name, description,
+//     language, and forks count in the list.
 
-const RepoListItem = (props) => {
-  return <li>a</li>;
+const testData = {
+  name: 'Test Name',
+  language: 'Test Language',
+  'fork-count': 5,
+};
+
+const RepoListItem = ({ repoInformation }) => {
+  return (
+    <li>{`${testData.name} ${testData.language} ${testData['fork-count']}`}</li>
+  );
 };
 
 export default RepoListItem;
